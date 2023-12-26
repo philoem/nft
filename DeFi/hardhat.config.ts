6a653bf240a1f7d75e842148fac747add4e05f26
Philoem <philoem24@gmail.com>
2023-12-26 14:51:40 +0100

    build: add package dotenv + new script to run hardhat server

--- DeFi/hardhat.config.ts
+++ DeFi/hardhat.config.ts
@@ -1,5 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
+
+const ALCHEMY_RPC_URL = process.env.ALCHEMY_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -19,7 +24,18 @@ const config: HardhatUserConfig = {
         },
       }
     ]
-  }, 
+  },  
+  defaultNetwork: "matic",
+  networks: {
+    matic: {
+      url: ALCHEMY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
