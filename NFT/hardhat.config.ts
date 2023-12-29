a99d76ce5a0f0cf92de62eb4e8c876bacd3cb525
Philoem <philoem24@gmail.com>
2023-12-29 15:18:58 +0100

    build: nft: add packages + init project
    add dotenv + init hardhat + set constants to connect testnet sepolia

---
+++ NFT/hardhat.config.ts
@@ -0,0 +1,35 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      }
+    ]
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: INFURA_API_KEY,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  }, 
+};
+
+export default config;
