1fabe9afdf67312ac581dd70c886f5efd691f42c
Philoem <philoem24@gmail.com>
2023-12-30 12:04:24 +0100

    feat: add contract + remove unused contract

---
+++ NFT/contracts/PhiloemCollector.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract PhiloemCollection is ERC721, ERC721URIStorage {
+    constructor() ERC721("PhiloemCollection", "PHC") {
+        for(uint i = 0; i < 5; i++) {
+            _mint(msg.sender, i);
+        }
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmU4vBVqXu7DHW6fvGt74ZrS2SQ2Pr8hc9XZTabgsaW3hw";
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        pure
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return string(abi.encodePacked(_baseURI(), Strings.toString(tokenId), ".json"));
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
